e84999a0aed373ec9f056a4d88cc1ad022dd88eb
CSEON <ashabsadaev360@gmail.com>
2022-06-30 17:39:53 +0300

    Добавьте файлы проекта.

---
+++ Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using RPGRandom;
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class A
+    {
+        
+    }
+}
